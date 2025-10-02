236335502378e7bfc6e571cc14fb1fcec582c7a4
UniOreoX <uox2004@gmail.com>
2025-10-02 15:51:10 +0800

    initial commit

---
+++ app/src/main/java/edu/csu/dynamicyouth/MainActivity.kt
@@ -0,0 +1,196 @@
+package edu.csu.dynamicyouth
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import edu.csu.dynamicyouth.ui.theme.DynamicYouthTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DynamicYouthTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(
+                                    text = stringResource(id = R.string.app_name),
+                                    textAlign = TextAlign.Center,
+                                    modifier = Modifier.fillMaxWidth()
+                                )
+                            }
+                        )
+                    }
+                ) { innerPadding ->
+                    Box(modifier = Modifier.padding(innerPadding))
+                    {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(128.dp)
+                        ) {
+                            Card(
+                                colors = CardDefaults.cardColors(
+                                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                                ),
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(start = 3.dp, top = 3.dp, end = 3.dp, bottom = 3.dp)
+                                    .weight(1f)
+                            ) {
+                                Text(
+                                    text = "主页",
+                                    modifier = Modifier
+                                        .padding(all = 16.dp),
+                                    style = MaterialTheme.typography.headlineLarge,
+                                    textAlign = TextAlign.Center,
+                                )
+                                Text(
+                                    text = "悦动青春登山打卡",
+                                    modifier = Modifier
+                                        .padding(start = 16.dp, end = 16.dp, bottom = 16.dp),
+                                    style = MaterialTheme.typography.bodyLarge,
+                                    textAlign = TextAlign.Center,
+                                )
+                            }
+                            Card(
+                                colors = CardDefaults.cardColors(
+                                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                                ),
+                                modifier = Modifier
+                                    .padding(top = 3.dp, end = 3.dp, bottom = 3.dp)
+                                    .fillMaxSize()
+                                    .weight(1f)
+                            ) {
+                                Text(
+                                    text = "北京时间",
+                                    modifier = Modifier
+                                        .padding(16.dp),
+                                    style = MaterialTheme.typography.headlineLarge,
+                                    textAlign = TextAlign.Center,
+                                )
+                                Text(
+                                    text = "北京时间",
+                                    modifier = Modifier
+                                        .padding(start = 16.dp, end = 16.dp, bottom = 16.dp),
+                                    style = MaterialTheme.typography.bodyLarge,
+                                    textAlign = TextAlign.Center,
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DynamicYouthTheme {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            topBar = {
+                Text(
+                    text = stringResource(id = R.string.app_name),
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.headlineMedium,
+                    modifier = Modifier.fillMaxWidth().padding(4.dp)
+                )
+            }
+        ) { innerPadding ->
+            Box(modifier = Modifier.padding(innerPadding))
+            {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(128.dp)
+                ) {
+                    Card(
+                        colors = CardDefaults.cardColors(
+                            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                        ),
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(start = 6.dp, top = 3.dp, bottom = 3.dp)
+                            .weight(1f)
+                    ) {
+                        Text(
+                            text = "主页",
+                            modifier = Modifier
+                                .padding(all = 16.dp),
+                            style = MaterialTheme.typography.headlineLarge,
+                            textAlign = TextAlign.Center,
+                        )
+                        Text(
+                            text = "中南大学登山打卡",
+                            modifier = Modifier
+                                .padding(start = 16.dp, end = 16.dp, bottom = 16.dp),
+                            style = MaterialTheme.typography.bodyLarge,
+                            textAlign = TextAlign.Center,
+                        )
+                    }
+
+                    Card(
+                        colors = CardDefaults.cardColors(
+                            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                        ),
+                        modifier = Modifier
+                            .padding(top = 3.dp, end = 6.dp, bottom = 3.dp)
+                            .fillMaxSize()
+                            .weight(1f)
+                    ) {
+                        Text(
+                            text = "北京时间",
+                            modifier = Modifier
+                                .padding(16.dp),
+                            style = MaterialTheme.typography.headlineLarge,
+                            textAlign = TextAlign.Center,
+                        )
+                        Text(
+                            text = "北京时间",
+                            modifier = Modifier
+                                .padding(start = 16.dp, end = 16.dp, bottom = 16.dp),
+                            style = MaterialTheme.typography.bodyLarge,
+                            textAlign = TextAlign.Center,
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
