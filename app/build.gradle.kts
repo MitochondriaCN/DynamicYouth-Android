51cda1e50b02bb5cefcb9dbdfcfcfd3f8235608d
线粒体XianlitiCN <mitochondriacn@gmail.com>
2025-10-03 18:03:43 +0800

    fix: 解决因缺少viewmodel依赖导致NavHost初始化报错的bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,8 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.4") // 建议使用最新稳定版
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.4")
+
 }
