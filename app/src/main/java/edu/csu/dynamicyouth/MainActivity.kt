5707b9ae7114596ece416c6ee9830651a8d39cde
线粒体XianlitiCN <mitochondriacn@gmail.com>
2025-10-03 16:12:01 +0800

    feat: 搓一个大致的首页框架出来

--- app/src/main/java/edu/csu/dynamicyouth/MainActivity.kt
+++ app/src/main/java/edu/csu/dynamicyouth/MainActivity.kt
@@ -6,26 +6,36 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import edu.csu.dynamicyouth.ui.theme.DynamicYouthTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,73 +50,20 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     topBar = {
                         TopAppBar(
+                            modifier = Modifier.padding(start = 10.dp, top = 10.dp),
                             title = {
                                 Text(
                                     text = stringResource(id = R.string.app_name),
-                                    textAlign = TextAlign.Center,
+                                    textAlign = TextAlign.Start,
+                                    fontWeight = FontWeight.Bold,
+                                    fontSize = 30.sp,
                                     modifier = Modifier.fillMaxWidth()
                                 )
                             }
                         )
                     }
                 ) { innerPadding ->
-                    Box(modifier = Modifier.padding(innerPadding))
-                    {
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(128.dp)
-                        ) {
-                            Card(
-                                colors = CardDefaults.cardColors(
-                                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
-                                ),
-                                modifier = Modifier
-                                    .fillMaxSize()
-                                    .padding(start = 3.dp, top = 3.dp, end = 3.dp, bottom = 3.dp)
-                                    .weight(1f)
-                            ) {
-                                Text(
-                                    text = "主页",
-                                    modifier = Modifier
-                                        .padding(all = 16.dp),
-                                    style = MaterialTheme.typography.headlineLarge,
-                                    textAlign = TextAlign.Center,
-                                )
-                                Text(
-                                    text = "悦动青春登山打卡",
-                                    modifier = Modifier
-                                        .padding(start = 16.dp, end = 16.dp, bottom = 16.dp),
-                                    style = MaterialTheme.typography.bodyLarge,
-                                    textAlign = TextAlign.Center,
-                                )
-                            }
-                            Card(
-                                colors = CardDefaults.cardColors(
-                                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
-                                ),
-                                modifier = Modifier
-                                    .padding(top = 3.dp, end = 3.dp, bottom = 3.dp)
-                                    .fillMaxSize()
-                                    .weight(1f)
-                            ) {
-                                Text(
-                                    text = "北京时间",
-                                    modifier = Modifier
-                                        .padding(16.dp),
-                                    style = MaterialTheme.typography.headlineLarge,
-                                    textAlign = TextAlign.Center,
-                                )
-                                Text(
-                                    text = "北京时间",
-                                    modifier = Modifier
-                                        .padding(start = 16.dp, end = 16.dp, bottom = 16.dp),
-                                    style = MaterialTheme.typography.bodyLarge,
-                                    textAlign = TextAlign.Center,
-                                )
-                            }
-                        }
-                    }
+                    DynamicYouthHomePage(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -121,6 +78,41 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+/**
+ * 探索：首页
+ */
+@Composable
+fun DynamicYouthHomePage(modifier: Modifier = Modifier) {
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = modifier
+            .verticalScroll(scrollState)
+            .padding(20.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = null,
+            modifier = modifier.fillMaxWidth()
+        )
+
+        Text(
+            text = "这里是地图",
+            modifier
+                .fillMaxWidth()
+        )
+
+        Text(text = stringResource(R.string.climbing_description))
+
+        Button(
+            modifier = Modifier
+                .fillMaxWidth(),
+            onClick = { /*TODO*/ },
+        ) {
+            Text(text = stringResource(R.string.begin_climb))
+        }
+    }
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
@@ -129,29 +121,22 @@ fun GreetingPreview() {
         Scaffold(
             modifier = Modifier.fillMaxSize(),
             topBar = {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(20.dp)
-                ) {
-                    Text(
-                        text = stringResource(id = R.string.app_name),
-                        textAlign = TextAlign.Start,
-                        fontWeight = FontWeight.Bold,
-                        style = MaterialTheme.typography.headlineMedium,
-                        modifier = Modifier
-                            .padding(start = 4.dp)
-                    )
-                }
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                ) {}
+                TopAppBar(
+                    title = {
+                        Text(
+                            text = stringResource(id = R.string.app_name),
+                            textAlign = TextAlign.Start,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 30.sp,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(10.dp)
+                        )
+                    }
+                )
             }
         ) { innerPadding ->
-            Box(modifier = Modifier.padding(innerPadding))
-            {
-            }
+            DynamicYouthHomePage(modifier = Modifier.padding(innerPadding))
         }
     }
 }
