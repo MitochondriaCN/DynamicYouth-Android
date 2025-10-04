b72be18c7f375150fa81f9df52acc0c8a96e7287
线粒体XianlitiCN <mitochondriacn@gmail.com>
2025-10-04 16:08:32 +0800

    fix: 修复分支问题

--- app/src/main/java/edu/csu/dynamicyouth/MainActivity.kt
+++ app/src/main/java/edu/csu/dynamicyouth/MainActivity.kt
@@ -22,26 +22,40 @@ import androidx.compose.material.icons.filled.Contacts
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarDefaults
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
+import com.tencent.tencentmap.mapsdk.maps.MapView
+import edu.csu.dynamicyouth.component.AnnouncementCard
 import edu.csu.dynamicyouth.component.AppFrameworkBottomBar
 import edu.csu.dynamicyouth.component.AppFrameworkTopBar
-import edu.csu.dynamicyouth.screen.EventScreen
-import edu.csu.dynamicyouth.screen.HomeScreen
-import edu.csu.dynamicyouth.screen.ProfileScreen
-import edu.csu.dynamicyouth.screen.RankingScreen
 import edu.csu.dynamicyouth.ui.theme.DynamicYouthTheme
 
 class MainActivity : ComponentActivity() {
@@ -81,7 +95,7 @@ fun AppFramework(modifier: Modifier = Modifier) {
         ),
         BottomNavItem(
             icon = Icons.Filled.Contacts,
-            name = stringResource(R.string.profile),
+            name = stringResource(R.string.my),
             route = "profile"
         )
     )
@@ -99,15 +113,101 @@ fun AppFramework(modifier: Modifier = Modifier) {
                     .padding(innerPadding)
                     .padding(horizontal = 20.dp)
             ) {
-                composable("homepage") { HomeScreen() }
-                composable("ranking") { RankingScreen() }
-                composable("event") { EventScreen() }
-                composable("profile") { ProfileScreen() }
+                composable("homepage") { HomePage() }
+                composable("ranking") { RankingPage() }
+                composable("event") { EventPage() }
+                composable("profile") { ProfilePage() }
             }
         }
     }
-
 }
+
+
+@Composable
+fun HomePage(modifier: Modifier = Modifier) {
+    val scrollState = rememberScrollState()
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(
+                top = 3.dp,
+                bottom = 3.dp
+            )
+    ) {
+        Column(
+            modifier = modifier
+                .verticalScroll(scrollState)
+                .fillMaxSize()
+        ) {
+            AnnouncementCard(
+                modifier.fillMaxWidth(),
+                title = "悦动青春Android APP正在开发中",
+                content = "盛世升华，服务中南，不服务中南怎么行呢？按理说你这个级别的同志，还无权服务中南，不知道怎么回事，就把我调到升华工作室来了。很惭愧，就做了一点微小的贡献。"
+            )
+
+            Spacer(modifier = Modifier.padding(8.dp))
+
+            AsyncImage(
+                model = R.drawable.home_promotional,
+                contentDescription = stringResource(R.string.promotion_desc),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(16.dp))
+            )
+
+            Text(
+                text = "这里是地图",
+                Modifier
+                    .fillMaxWidth()
+            )
+
+            AndroidView(
+                modifier = Modifier.fillMaxWidth(),
+                factory = { context ->
+                    MapView(context)
+                })
+
+            Text(text = stringResource(R.string.climbing_description))
+
+
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter),
+        ) {
+            Spacer(
+                modifier.width(8.dp)
+            )
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                onClick = { /*TODO*/ },
+            ) {
+                Text(text = stringResource(R.string.begin_climb))
+            }
+            Spacer(
+                modifier.width(8.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun EventPage() {
+    Text(stringResource(R.string.event))
+}
+
+@Composable
+fun RankingPage() {
+    Text(stringResource(R.string.ranking))
+}
+
+@Composable
+fun ProfilePage() {
+    Text(stringResource(R.string.my))
+}
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
