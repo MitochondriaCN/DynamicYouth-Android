21f2b22cc84e76a96f6b913db5af499535d42b2c
UniOreoX <uox2004@gmail.com>
2025-10-05 18:07:08 +0800

    feat: 添加权限申请弹窗，优化import

--- app/src/main/java/edu/csu/dynamicyouth/MainActivity.kt
+++ app/src/main/java/edu/csu/dynamicyouth/MainActivity.kt
@@ -1,5 +1,7 @@
 package edu.csu.dynamicyouth
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,7 +15,13 @@ import androidx.compose.material.icons.filled.Contacts
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
@@ -22,14 +30,17 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.tencent.map.geolocation.TencentLocationManager
 import com.tencent.tencentmap.mapsdk.maps.TencentMapInitializer
 import edu.csu.dynamicyouth.component.AppFrameworkBottomBar
 import edu.csu.dynamicyouth.component.AppFrameworkTopBar
+import edu.csu.dynamicyouth.component.PermissionDialog
 import edu.csu.dynamicyouth.page.EventPage
 import edu.csu.dynamicyouth.page.HomePage
 import edu.csu.dynamicyouth.page.ProfilePage
 import edu.csu.dynamicyouth.page.RankingPage
 import edu.csu.dynamicyouth.ui.theme.DynamicYouthTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,6 +50,8 @@ class MainActivity : ComponentActivity() {
         // 同意腾讯地图隐私政策
         TencentMapInitializer.setAgreePrivacy(this, true)
         TencentMapInitializer.start(this)
+        // 同意腾讯位置服务隐私政策
+        TencentLocationManager.setUserAgreePrivacy(true)
         setContent {
             AppFramework()
         }
@@ -50,10 +63,40 @@ data class BottomNavItem(val name: String, val route: String, val icon: ImageVec
 /**
  * 主程序框架
  */
-@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("CoroutineCreationDuringComposition")
 @Composable
 fun AppFramework(modifier: Modifier = Modifier) {
     val navController = rememberNavController()
+    val scope = rememberCoroutineScope()
+    val snackBarHostState = remember { SnackbarHostState() }
+    val isDialogShown = remember { mutableStateOf(false) }
+
+    val deniedMessage = stringResource(R.string.permission_denied)
+
+    PermissionDialog(
+        permissions =
+            mapOf(
+                Manifest.permission.ACCESS_FINE_LOCATION to stringResource(R.string.ACCESS_FINE_LOCATION),
+                Manifest.permission.ACCESS_COARSE_LOCATION to stringResource(R.string.ACCESS_COARSE_LOCATION),
+            ),
+        policies =
+            listOf(
+                stringResource(R.string.TencentMapSDK_Permission),
+                stringResource(R.string.TencentLocationSDK_Permission),
+            ),
+        isShown = isDialogShown.value,
+        onDismissRequest = {
+            isDialogShown.value = true
+            scope.launch {
+                snackBarHostState.showSnackbar(
+                    message = deniedMessage,
+                    duration = SnackbarDuration.Long,
+                    withDismissAction = true
+                )
+            }
+        }
+    )
+
     val navigationItems = listOf(
         BottomNavItem(
             icon = Icons.Filled.Home,
@@ -80,6 +123,9 @@ fun AppFramework(modifier: Modifier = Modifier) {
     DynamicYouthTheme {
         Scaffold(
             modifier = modifier.fillMaxSize(),
+            snackbarHost = {
+                SnackbarHost(hostState = snackBarHostState)
+            },
             topBar = { AppFrameworkTopBar() },
             bottomBar = { AppFrameworkBottomBar(navController, navigationItems) }
         ) { innerPadding ->
