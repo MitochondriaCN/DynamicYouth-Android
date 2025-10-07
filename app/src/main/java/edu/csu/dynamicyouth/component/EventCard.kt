43330837ade1b60ffca6026b7cdafcaf20c27ef9
线粒体XianlitiCN <mitochondriacn@gmail.com>
2025-10-07 19:25:01 +0800

    feat: 搓一个活动卡片

---
+++ app/src/main/java/edu/csu/dynamicyouth/component/EventCard.kt
@@ -0,0 +1,85 @@
+package edu.csu.dynamicyouth.component
+
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+
+/**
+ * 活动卡片。该卡片默认是收起状态，第一次点击会转换为扩展状态，第二次点击会调用`onSecondClick()`。
+ * @param image 活动图片。
+ */
+@Composable
+fun EventCard(
+    modifier: Modifier = Modifier,
+    image: Any,
+    title: String,
+    description: String,
+    onSecondClick: () -> Unit = {}
+) {
+    val animationDurationMillis = 300
+    var isExpanded by remember { mutableStateOf(false) }
+    val animatedRatio by animateFloatAsState(
+        targetValue = if (isExpanded) 16f / 9f else 8f / 3f,
+        animationSpec = tween(animationDurationMillis)
+    )
+
+    Card(
+        modifier = modifier
+            .animateContentSize(tween(animationDurationMillis))
+            .clickable { if (isExpanded) onSecondClick() else isExpanded = true }
+    ) {
+        Column {
+            AsyncImage(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(ratio = animatedRatio),
+                model = image,
+                contentDescription = null,
+                contentScale = ContentScale.Crop
+            )
+            Column(
+                modifier = Modifier.padding(15.dp)
+            ) {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 2,
+                    softWrap = true,
+                    overflow = TextOverflow.Ellipsis,
+                )
+
+                if (isExpanded) {
+                    Spacer(modifier = Modifier.padding(3.dp))
+                    Text(
+                        text = description,
+                        style = MaterialTheme.typography.bodyMedium,
+                        maxLines = 3,
+                        softWrap = true,
+                        overflow = TextOverflow.Ellipsis,
+                    )
+                }
+            }
+        }
+    }
+}
