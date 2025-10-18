c4774f80bd70a58820651af09a0a67545be64dde
线粒体XianlitiCN <mitochondriacn@gmail.com>
2025-10-18 13:54:22 +0800

    feat: Hilt、Retrofit及DI基础设施：Pt. VI - 完成TokenManager

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation(libs.converter.kotlinx.serialization)
     implementation(libs.retrofit)
     implementation(libs.kotlinx.serialization.json)
+    implementation(libs.androidx.security.crypto)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/edu/csu/dynamicyouth/AuthActivity.kt
+++ app/src/main/java/edu/csu/dynamicyouth/AuthActivity.kt
@@ -1,5 +1,6 @@
 package edu.csu.dynamicyouth
 
+import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.util.Log
@@ -27,12 +28,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
+import dagger.hilt.android.AndroidEntryPoint
+import edu.csu.dynamicyouth.network.TokenManager
 import edu.csu.dynamicyouth.ui.theme.DynamicYouthTheme
+import javax.inject.Inject
 
 /**
  * 认证Activity。
  */
+@AndroidEntryPoint
 class AuthActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var tokenManager: TokenManager
 
     val AUTH_URL = "https://ydqc.csu.edu.cn/api/v1/oauth2/authorization/csu"
 
@@ -43,80 +51,78 @@ class AuthActivity : ComponentActivity() {
             AuthPage(AUTH_URL)
         }
     }
-}
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun AuthPage(url: String, modifier: Modifier = Modifier) {
-    DynamicYouthTheme {
-        Scaffold(
-            modifier = Modifier.fillMaxSize(),
-            topBar = {
-                TopAppBar(
-                    title = { Text(stringResource(R.string.title_activity_auth)) },
-                    navigationIcon = {
-                        IconButton(onClick = { finishWithoutToken() }) {
-                            Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null)
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun AuthPage(url: String, modifier: Modifier = Modifier) {
+        DynamicYouthTheme {
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                topBar = {
+                    TopAppBar(
+                        title = { Text(stringResource(R.string.title_activity_auth)) },
+                        navigationIcon = {
+                            IconButton(onClick = { finish() }) {
+                                Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null)
+                            }
+                        }
+                    )
+                }
+            ) { innerPadding ->
+                AndroidView(
+                    modifier = Modifier
+                        .padding(innerPadding)
+                        .fillMaxSize(),
+                    factory = { context ->
+                        WebView(context).apply {
+                            settings.javaScriptEnabled = true
+                            settings.domStorageEnabled = true
+                            webViewClient = object : WebViewClient() {
+
+                                override fun shouldOverrideUrlLoading(
+                                    view: WebView?,
+                                    request: WebResourceRequest?
+                                ): Boolean {
+                                    Log.d("DEV", "shouldOverrideUrlLoading: ${request?.url}")
+
+                                    // 参考URL:
+                                    // https://ydqc.csu.edu.cn/api/v1/login/oauth2/code/csu?code=OC3778MqnTjk0GZkacjTOtuocIsKviTuUH25&state=JZrQ-DEE528JtQDrBZ0dbXZFYrp0za7t2nCXaB1vg1Y%3D
+                                    if (request?.url.toString()
+                                            .contains("https://ydqc.csu.edu.cn/api/v1/login/oauth2/code/csu?code")
+                                    ) {
+                                        val cookies = CookieManager.getInstance()
+                                            .getCookie("https://ydqc.csu.edu.cn")
+                                        Log.d("DEV", "Cookies: $cookies")
+                                        //找到cookies中带有token=...的字符串
+                                        val tokenPart =
+                                            cookies.split(';').find { it.contains("token=") }
+                                        //获取等号后面的部分
+                                        val token = tokenPart?.split('=')?.get(1)
+
+                                        tokenManager.sendTokenResult(token)
+                                        finish()
+                                    }
+                                    return false
+                                }
+
+                                override fun shouldOverrideUrlLoading(
+                                    view: WebView?,
+                                    url: String?
+                                ): Boolean {
+                                    return false
+                                }
+                            }
+                            loadUrl(url)
                         }
                     }
                 )
             }
-        ) { innerPadding ->
-            AndroidView(
-                modifier = Modifier
-                    .padding(innerPadding)
-                    .fillMaxSize(),
-                factory = { context ->
-                    WebView(context).apply {
-                        settings.javaScriptEnabled = true
-                        settings.domStorageEnabled = true
-                        webViewClient = object : WebViewClient() {
-
-                            override fun shouldOverrideUrlLoading(
-                                view: WebView?,
-                                request: WebResourceRequest?
-                            ): Boolean {
-                                Log.d("DEV", "shouldOverrideUrlLoading: ${request?.url}")
-
-                                // 参考URL:
-                                // https://ydqc.csu.edu.cn/api/v1/login/oauth2/code/csu?code=OC3778MqnTjk0GZkacjTOtuocIsKviTuUH25&state=JZrQ-DEE528JtQDrBZ0dbXZFYrp0za7t2nCXaB1vg1Y%3D
-                                if (request?.url.toString()
-                                        .contains("https://ydqc.csu.edu.cn/api/v1/login/oauth2/code/csu?code") == true
-                                ) {
-                                    val cookies = CookieManager.getInstance()
-                                        .getCookie("https://ydqc.csu.edu.cn")
-                                    Log.d("DEV", "Cookies: $cookies")
-                                    //找到cookies中带有token=...的字符串
-                                    val tokenPart = cookies.split(';').find { it.contains("token=") }
-                                    //获取等号后面的部分
-                                    val token = tokenPart?.split('=')?.get(1)
-                                    Log.d("DEV", "Token: $token")
-                                }
-                                return false
-                            }
-
-                            override fun shouldOverrideUrlLoading(
-                                view: WebView?,
-                                url: String?
-                            ): Boolean {
-                                return false
-                            }
-                        }
-                        loadUrl(url)
-                    }
-                }
-            )
         }
     }
-}
 
-fun finishWithoutToken() {
-    TODO("Not yet implemented")
-}
+    private fun finish(token: String?) {
+        tokenManager.sendTokenResult(token)
+        finish()
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun AuthActivityPreview() {
-    //这个预览可能没办法渲染，但是编译出来是好的，所以请酌情使用
-    AuthPage("https://ydqc.csu.edu.cn/api/v1/oauth2/authorization/csu")
 }
